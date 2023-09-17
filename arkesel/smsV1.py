58a330eda6ced589503d4236cb826846b154a5c8
aglili <cecilselorm34@gmail.com>
2023-09-17 21:35:05 +0000

    added docstring and type req

--- arkesel/smsV1.py
+++ arkesel/smsV1.py
@@ -1,51 +1,105 @@
 import requests
 
 
+import requests
+from typing import Optional
 
 class SMSV1:
-    def __init__(self,api_key) -> None:
+    """
+    A class to interact with the Arkesel SMS API for sending SMS messages and retrieving account information.
+
+    Attributes:
+        api_key (str): The API key for authentication.
+    """
+    def __init__(self, api_key: str) -> None:
+        """
+        Initializes an instance of the SMSV1 class.
+
+        Args:
+            api_key (str): The API key for authentication.
+        """
         self.api_key = api_key
 
+    def send_sms(self, sender: str, message: str, recipient: str) -> dict:
+        """
+        Sends an SMS message to the specified recipient.
 
-    def send_sms(self,sender:str,message:str,recipient:str):
+        Args:
+            sender (str): The sender's phone number or name.
+            message (str): The message content.
+            recipient (str): The recipient's phone number.
+
+        Returns:
+            dict: The API response in JSON format.
+        """
         url = "https://sms.arkesel.com/sms/api?action=send-sms"
-        params = {
-            "action":"send-sms",
-            "api_key":self.api_key,
-            "to":recipient,
-            "from":sender,
-            "sms":message
-        }
-        response = requests.get(url=url,params=params)
-        return response.json()
-    
-
-    def schedule_sms(self,recipeient:str,sender:str,message:str,time:str):
-        url = "https://sms.arkesel.com/sms/api?schedule=####"
         params = {
             "action": "send-sms",
             "api_key": self.api_key,
-            "to": recipeient,
+            "to": recipient,
+            "from": sender,
+            "sms": message
+        }
+        response = requests.get(url=url, params=params)
+        return response.json()
+
+    def schedule_sms(self, recipient: str, sender: str, message: str, time: str) -> dict:
+        """
+        Schedule an SMS message to be sent at a specific time.
+
+        Args:
+            recipient (str): The recipient's phone number.
+            sender (str): The sender's phone number or name.
+            message (str): The message content.
+            time (str): The time to schedule the SMS (format: "YYYY-MM-DD HH:MM:SS").
+
+        Returns:
+            dict: The API response in JSON format.
+        """
+        url = f"https://sms.arkesel.com/sms/api?schedule={time}"
+        params = {
+            "action": "send-sms",
+            "api_key": self.api_key,
+            "to": recipient,
             "from": sender,
             "sms": message,
             "schedule": time
         }
-        response = requests.get(url=url,params=params)
+        response = requests.get(url=url, params=params)
         return response.json()
-    
 
-    def check_balance(self):
+    def check_balance(self) -> dict:
+        """
+        Retrieves the account balance.
+
+        Returns:
+            dict: The API response in JSON format containing balance information.
+        """
         url = "https://sms.arkesel.com/sms/api?action=check-balance"
         params = {
             "action": "check-balance",
             "api_key": self.api_key,
             "response": "json"
         }
-        response = requests.get(url=url,params=params)
+        response = requests.get(url=url, params=params)
         return response.json()
 
+    def save_contact(self, phonebook_name: str, phone_number: str, first_name: Optional[str] = None,
+                     last_name: Optional[str] = None, email: Optional[str] = None, company: Optional[str] = None) -> dict:
+        """
+        Saves a contact to the specified phone book.
 
-    def save_contact(self,phonebook_name:str,phone_number:str,first_name:str=None,last_name:str=None,email:str=None,company:str=None):
+        Args:
+            phonebook_name (str): The name of the phone book.
+            phone_number (str): The phone number of the contact.
+            first_name (str, optional): The first name of the contact. Defaults to None.
+            last_name (str, optional): The last name of the contact. Defaults to None.
+            email (str, optional): The email address of the contact. Defaults to None.
+            company (str, optional): The company name of the contact. Defaults to None.
+
+        Returns:
+            dict: The API response in JSON format.
+        """
         url = "https://sms.arkesel.com/contacts/api"
         params = {
             "action": "subscribe-us",
@@ -55,14 +109,13 @@ class SMSV1:
         }
 
         if first_name:
-            params["first_name"]=first_name
+            params["first_name"] = first_name
         if last_name:
-            params['last_name']=last_name
+            params["last_name"] = last_name
         if email:
-            params['email']=email
+            params["email"] = email
         if company:
-            params['company']=company
+            params["company"] = company
 
-        response = requests.get(url=url,params=params)
-
+        response = requests.get(url=url, params=params)
         return response.json()
--- arkesel/smsV2.py
+++ arkesel/smsV2.py
@@ -1,12 +1,39 @@
 import requests
 
 class SMSV2:
+
+    """
+    A class to send SMS messages using the Arkesel SMS API.
+
+    This Uses Version 2 of the API
+
+    Attributes:
+        api_key (str): The API key for authentication.
+    """
     def __init__(self,api_key:str) -> None:
+
+        """
+        Initializes an instance of the SMSV2 class.
+
+        Args:
+            api_key (str): The API key for authentication.
+        """
         self.headers = {"api-key":api_key}
 
 
 
     def send_sms(self,sender:str,message:str,recipients:list):
+        """
+        Sends an SMS message to the specified recipients.
+
+        Args:
+            sender (str): The sender's phone number or name.
+            message (str): The message content.
+            recipients (List[str]): List of recipient phone numbers.
+
+        Returns:
+            dict: The API response in JSON format.
+        """
         url = "https://sms.arkesel.com/api/v2/sms/send"
         data = {
         "sender": sender,
@@ -21,6 +48,18 @@ class SMSV2:
 
     def schedule_sms(self,sender:str,recipients:list,message:str,schedule_date:str):
 
+        """
+        Schedule an SMS message to be sent at a specific date and time.
+
+        Args:
+            sender (str): The sender's phone number or name.
+            recipients (list): List of recipient phone numbers.
+            message (str): The message content.
+            schedule_date (str): The date and time to schedule the SMS (format: "YYYY-MM-DD HH:MM:SS").
+
+        Returns:
+            dict: The API response in JSON format.
+    """
         url = "https://sms.arkesel.com/api/v2/sms/send"
 
 
@@ -38,11 +77,26 @@ class SMSV2:
     
 
     def check_balance(self):
+        """
+        Retrieves the account balance and other related details.
+
+        Returns:
+            dict: The API response in JSON format containing balance details.
+        """
         url = "https://sms.arkesel.com/api/v2/clients/balance-details"
         response = requests.get(url=url,headers=self.headers)
         return response.json()
     
     def sms_details(self,sms_id:str):
+        """
+        Retrieves details of a specific SMS using its ID.
+
+        Args:
+            sms_id (str): The ID of the SMS.
+
+        Returns:
+            dict: The API response in JSON format containing SMS details.
+        """
         url = f"https://sms.arkesel.com/api/v2/sms{sms_id}"
         response = requests.get(url=url,headers=self.headers)
         return response.json()
